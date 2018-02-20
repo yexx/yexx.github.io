72b4be5cf6b8669ee56c799b6c5658823ec92c91
Yeshua Braz <yeshua.braz@etools.com.br>
2018-02-20 12:30:26 -0300

    Responsive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-$(window).on("load",function(){var o=[];$(".videos video").each(function(e,i){o.push($(this).attr("id")),console.log(o)});var e=$(".videos video").length,i=1;setInterval(function(){$(".videos video").hide(),$(".videos video").eq(i-1).show(),i>=e?i=0:i++},5e3)});
+$(window).on("load",function(){var e=$(".videos video").length,o=1;setInterval(function(){$(".videos video").hide(),$(".videos video").eq(o-1).show(),o>=e?o=0:o++},5e3)});
