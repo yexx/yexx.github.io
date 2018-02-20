9de9004d6964e9d48c0d9d75a7e00cc2a66968a6
Yeshua Braz <yeshua.braz@etools.com.br>
2018-02-20 18:39:16 -0300

    Alteraçoes no estilo do texto

--- prod/resources/scripts/main.js
+++ prod/resources/scripts/main.js
@@ -2,15 +2,15 @@
 
 /* Funçoes Gerais */
 $(window).on('load',function(){
-	var count = $('.videos video').length
-	var ref = 1;
-	setInterval(function(){
-		$('.videos video').hide();
-		$('.videos video').eq(ref-1).show();
-		if(ref >= count){
-			ref = 0;
-		} else {
-			ref ++;
-		}
-	},5000)
+	// var count = $('.videos video').length
+	// var ref = 1;
+	// setInterval(function(){
+	// 	$('.videos video').hide();
+	// 	$('.videos video').eq(ref-1).show();
+	// 	if(ref >= count){
+	// 		ref = 0;
+	// 	} else {
+	// 		ref ++;
+	// 	}
+	// },5000)
 });
