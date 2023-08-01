adc95bd537ace76c54e69262cb37b60f07677aa1
Yeshua Braz <yexords@gmail.com>
2023-08-01 16:15:37 -0300

    f885c33

---
+++ _nuxt/6356322.modern.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{281:function(e,n,t){"use strict";t.r(n);var c=t(15),component=Object(c.a)({},(function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)}),[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",[t("p",[e._v("Blog")])])}],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/6602c04.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{353:function(e,n,t){"use strict";t.r(n);var c=t(24),component=Object(c.a)({},(function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)}),[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",[t("p",[e._v("You can reach me here:")])])}],!1,null,null,null);n.default=component.exports}}]);
